aeff7bad4b0acc45e07d94aa8046f94d9acf3a37
exam40 <aydenchew11@gmail.com>
2026-08-05 23:39:05 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const VERSION = 'exam-planner-v1';
+const CACHE = VERSION;
+const CORE = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await Promise.all(CORE.map(async (url) => {
+      try { const res = await fetch(url); if (res.ok) await cache.put(url, res); } catch {}
+    }));
+    await self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== 'GET' || url.origin !== location.origin) return;
+
+  if (url.pathname.endsWith('/script.js') || url.pathname.endsWith('/style.css') || url.pathname.endsWith('/index.html')) {
+    event.respondWith(caches.match(event.request).then((hit) => hit || fetch(event.request).then((res) => { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(event.request, copy)); return res; })));
+    return;
+  }
+
+  event.respondWith(fetch(event.request).then((res) => { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(event.request, copy)); return res; }).catch(() => caches.match(event.request)));
+});
